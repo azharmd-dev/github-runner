bb7e1f25476e44d41bd733625e6466989a863e95
azharmd-dev <azharmd0794@gmail.com>
2026-02-26 09:52:37 +0530

    github action self hosted runner setup

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "robomart"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "azharprojects.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0553382WPE1E3VPCYS6"
+  description = "description"
+}
